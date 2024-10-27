12077c5580ed06f0c47cd8222c778255b71e1b18
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-10-27 17:25:15 +0300

    toys lab

--- src/index.js
+++ src/index.js
@@ -13,3 +13,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// get request
+
+fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(toys => {
+    const toyCollection = document.getElementById("toy-collection");
+    toys.forEach(toy => {
+      const toyCard = document.createElement("div");
+      toyCard.className = "card";
+      const toyName = document.createElement("h2");
+      toyName.textContent = toy.name;
+      const toyImage = document.createElement("img");
+      toyImage.className = "toy-avatar";
+      toyImage.src = toy.image;
+      const toyLikes = document.createElement("p");
+      toyLikes.textContent = `Likes: ${toy.likes}`;
+      const likeBtn = document.createElement("button");
+      likeBtn.className = "like-btn";
+      likeBtn.id = `like-btn-${toy.id}`;
+      likeBtn.textContent = "Like ";
+      toyCard.append(toyName, toyImage, toyLikes, likeBtn);
+      toyCollection.appendChild(toyCard);
+    });
+  });
+  
